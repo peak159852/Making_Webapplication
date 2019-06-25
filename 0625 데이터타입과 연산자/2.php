5912c09707cbae402ac0d948947e15eec010496e
peak159852 <44461022+peak159852@users.noreply.github.com>
2019-06-25 17:52:12 +0900

    PHP,Js 에서 데이터 타입과 연산자 비교
    PHP 10+10 과 "10"+"10"은 20으로 같게 표현  "10"."10" 로 나타내면 1010 출력
    Js 10+10, "10"+"10" 다르게 출력

---
+++ 0625 데이터타입과 연산자/2.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h2>php</h2>
+    <?php
+        echo "10"."10"; // js와 차이 구분 echo 10+10;
+    ?>
+    <h2>js</h2>
+    <script>
+        document.write("10"+"10");
+    </script>
+</body>
+</html>
