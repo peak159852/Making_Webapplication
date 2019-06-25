ce6258622c1699bfbb4c15193aa9c7b80fedaf4a
peak159852 <44461022+peak159852@users.noreply.github.com>
2019-06-25 17:42:23 +0900

    PHP와 JavaScript 차이
    PHP코드는 서버에서 처리, JavaScript는 웹브라우저에서 처리.

---
+++ 0625 웹페이지에 코드 삽입하기/1.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h2>php</h2>
+    <?php
+        echo 10+10;
+    ?>
+    <h2>js</h2>
+    <script>
+        document.write("hello world");
+    </script>
+</body>
+</html>
