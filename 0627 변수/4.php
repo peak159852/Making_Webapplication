56df681aa42b2a839506d5fd72b6a3bd0865b91e
peak159852 <44461022+peak159852@users.noreply.github.com>
2019-06-27 17:15:27 +0900

    Js와 PHP에서의 변수사용
    PHP에서 문자와 문자간 연결은 . 으로 한다

---
+++ 0627 변수/4.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+        name = "Girin";
+        document.write("안녕하세요 " + name + " 입니다");
+    </script>
+    <h1>PHP</h1>
+    <?php
+        $name = "Girin";
+        echo "안녕하세요 ".$name." 입니다.";
+    ?>
+</body>
+</html>
