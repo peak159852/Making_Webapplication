54ce35e9cff920dfa751fc5a91f2fb89722ef069
peak159852 <44461022+peak159852@users.noreply.github.com>
2019-07-04 18:19:04 +0900

    php와 js에서 비교, 조건문
    둘의 형식 비교해보기

---
+++ 0704 비교, 조건문/5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Page Title</title>
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script charset = "utf-8">
+        document.write(1==2);
+    </script>
+    <h1>PHP</h1>
+    <?php
+        var_dump(1==1);
+    ?>
+</body>
+</html>
---
+++ 0704 비교, 조건문/6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script charset = "utf-8">
+        result = (1==1);
+        if(result){
+            document.write("참");
+        } else {
+            document.write("거짓");
+        }
+    </script>
+    <h1>PHP</h1>
+    <?php
+        $result = (2==2);
+        if($result){
+            ehco "참";
+        } else {
+            ehco "거짓";
+        }
+    ?>
+</body>
+</html>
