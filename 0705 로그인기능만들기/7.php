679f76ed0870d6aada6a3c071f293aeeb2d0b455
peak159852 <44461022+peak159852@users.noreply.github.com>
2019-07-05 15:29:03 +0900

    로그인 기능만들기
    php 와 js 에서 로그인 기능 만들기

---
+++ 0705 로그인기능만들기/7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Page Title</title>
+</head>
+<body>
+    <script>
+        password = prompt("비밀번호");
+        if(password == 1111){
+            document.write("어서오세요.");
+        } else {
+            document.wirte("뉘신지?");
+        }
+        
+    </script>
+</body>
+</html>
