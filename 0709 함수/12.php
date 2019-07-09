b464d0f1e547f8c98ca8588571ccad8923b3c640
peak159852 <44461022+peak159852@users.noreply.github.com>
2019-07-09 15:55:54 +0900

    함수
    php 와 js 에서의 함수사용

---
+++ 0709 함수/12.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+       function a(){
+           document.write("Hi i'm Girin");
+       }
+       a();
+    </script>
+    <h1>PHP</h1>
+    <?php
+       function a(){
+           echo "Hi i'm Girin";
+       } 
+       a();
+    ?>
+</body>
+</html>
---
+++ 0709 함수/13.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+       function a(input){
+           return input + 1;
+       }
+       document.write(a(5);;
+    </script>
+    <h1>PHP</h1>
+    <?php
+       function a($input){
+          return $input + 1;
+       } 
+       echo a(5);
+    ?>
+</body>
+</html>
